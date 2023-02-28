8b06543ef5b35d1bd393b649bfd55657556afafa
Aman Jain <amanjain4269@gmail.com>
2023-02-28 17:34:23 +0530

    pushing another file

---
+++ Stack.py
@@ -0,0 +1,12 @@
+#push method : adding item on the top in stack
+#peek method : returns the top most item or the last item in the stack
+#pop method : returns and delete the top most item or the last item in the stack
+#isEmpty : returns a boolean value to check whether the stack is empty or not (true/false)
+#size : to know the number of elements in the stack
+#stack follows LIFO principal i.e. last in first out
+#use case of the stack : 1) undo/redo operations i.e. to check the recent updations  2) bactracking while writing programs
+
+#stack can be implemented with the help of :
+#ARRAY......DYNAMIC ARRAY.....LINKED LIST
+
+# 1) 
